e3bbed921bd10fc7f9a9d6f108cb2e2e918afd8d
Bruno Marques <brunoa879@gmail.com>
2025-11-20 20:46:15 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/brunomarques/picpay_wallet_api/PicpayWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.brunomarques.picpay_wallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayWalletApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunomarques/picpay_wallet_api/PicpayWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomarques.picpay_wallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayWalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
